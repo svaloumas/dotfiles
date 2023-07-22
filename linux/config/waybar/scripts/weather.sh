79bd9ed34fb99fb9c7949a39181bfa41645fe22c
Stefanos Valoumas <svaloumas@gmail.com>
2023-07-22 16:24:15 +0300

    chore: update waybar weather script

--- linux/config/waybar/scripts/weather.sh
+++ linux/config/waybar/scripts/weather.sh
@@ -3,11 +3,10 @@
 LOC="$1"
 # HTML encode string as %20
 LOCATION=$(sed -e "s/ /%20/g" <<<"$LOC")
-content=$(curl -sS "https://thisdavej.azurewebsites.net/api/weather/current?loc=$LOCATION&deg=C")
+content=$(curl -sS "https://api.openweathermap.org/data/2.5/weather?q=Athens&APPID=<appid>&units=metric")
 ICON=$(curl -s 'https://wttr.in/?format=1' | sed 's/[+0-9a-cA-Z°-]//g' )
-# echo $ICON
-TEMP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)"' | sed 's/"//g')
-TOOLTIP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)\n\(.skytext)"' | sed 's/"//g')
+TEMP=$(echo $content | jq -r '. | "\(.main.temp)°"' | sed 's/"//g')
+TOOLTIP=$(echo $content | jq -r '. | "\(.main.temp)°\(.weather[0].main),\nfeels like \(.main.feels_like)"' | sed 's/"//g')
 CLASS=$(echo $content | jq .skytext)
-echo '{"text": "'$TEMP'", "tooltip": "'$ICON $TOOLTIP $LOC'", "class": '$CLASS' }'
+echo '{"text": "'$TEMP'", "tooltip": "'$ICON $TOOLTIP'", "class": '$CLASS' }'
 
